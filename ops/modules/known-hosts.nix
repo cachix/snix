6f7239b3b83380a2f1ba4de7f1e4188cc6606099
Vincent Ambo <mail@tazj.in>
2025-02-02 01:04:48 +0300

    fix(ops/known-hosts): add bugry & nevsky keys
    Without these, Gerrit replication is unhappy.
    
    Change-Id: Id0edbc6a1cf301f9ed7ef2a88ccb0ef70f469693
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/13089
    Tested-by: BuildkiteCI
    Reviewed-by: sterni <sternenseemann@systemli.org>

--- ops/modules/known-hosts.nix
+++ ops/modules/known-hosts.nix
@@ -13,6 +13,16 @@
       publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOag0XhylaTVhmT6HB8EN2Fv5Ymrc4ZfypOXONUkykTX";
     };
 
+    bugry = {
+      hostNames = [ "bugry.tvl.fyi" ];
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGqG6sITyJ/UsQ/RtYqmmMvTT4r4sppadoQIz5SvA+5J";
+    };
+
+    nevsky = {
+      hostNames = [ "nevsky.tvl.fyi" ];
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHQe7M+G8Id3ZD7j+I07TCUV1o12q1vpsOXHRlcPSEfa";
+    };
+
     github = {
       hostNames = [ "github.com" ];
       publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
