7b36b8e2851ee0251e59b156c989a439d93f29e3
Florian Klink <flokli@flokli.de>
2025-03-18 12:06:46 +0000

    chore(ops/keycloak): move oauth application to snix-project org
    This was a personal application before, now it's at the `snix-project`
    org.
    
    Change-Id: I6df9393f23593f58739f331e73103022301b4f11
    Reviewed-on: https://cl.snix.dev/c/snix/+/30101
    Autosubmit: Florian Klink <flokli@flokli.de>
    Tested-by: besadii
    Reviewed-by: Ryan Lahfa <masterancpp@gmail.com>

--- ops/keycloak/identity_providers.tf
+++ ops/keycloak/identity_providers.tf
@@ -26,7 +26,7 @@ resource "keycloak_oidc_identity_provider" "github" {
 resource "keycloak_oidc_identity_provider" "gitlab" {
   alias                 = "gitlab"
   provider_id           = "gitlab"
-  client_id             = "6ecb359ede53f7d80003d127dc4448bd1b1d73631a01273d9576e00ff9a94d2c"
+  client_id             = "aa15f85b418bde7549216c8d4ecf23849f667a9be496eebaed4b9cbafe17a176"
   client_secret         = var.gitlab_client_secret
   realm                 = keycloak_realm.snix.id
   backchannel_supported = false
