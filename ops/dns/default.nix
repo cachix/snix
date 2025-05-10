20589ef8cb99f3fd0aad58d10883be822fecfebe
Florian Klink <flokli@flokli.de>
2025-05-10 16:16:04 +0300

    fix(ops/dns): drop broken `checkZone` parts, fix validate
    There are no .zone files in here (maybe once were, before switching DNS
    providers, or this was copy-pasted from elsewhere).
    
    Also, the validate.terraform target was broken, due to a typo, and not
    covered in CI, due to being inside another attrset.
    
    There's only a single check left, so just call that one `validate`,
    making it consistent with other //ops terraform workspaces, and getting
    CI to actually check it.
    
    Change-Id: I022138d4d3c74181a53738cb53a48b7945392345
    Reviewed-on: https://cl.snix.dev/c/snix/+/30499
    Tested-by: besadii
    Reviewed-by: Ryan Lahfa <masterancpp@gmail.com>
    Autosubmit: Florian Klink <flokli@flokli.de>

--- ops/dns/default.nix
+++ ops/dns/default.nix
@@ -1,24 +1,14 @@
 { depot, lib, pkgs, ... }:
 
-let
-  checkZone = zone: file: pkgs.runCommand "${zone}-check" { } ''
-    ${pkgs.bind}/bin/named-checkzone -i local ${zone} ${file} | tee $out
-  '';
-
-in
 depot.nix.readTree.drvTargets rec {
   # Provide a Terraform wrapper with the right provider installed.
   terraform = pkgs.terraform.withPlugins (p: [
     p.digitalocean
   ]);
 
-  validate = {
-    snix-dev = checkZone "snix.dev" ./snix.dev.zone;
-    snix-systems = checkZone "snix.systems" ./snix.systems.zone;
-    terraform = depot.tools.checks.validateTerrform {
-      inherit terraform;
-      name = "dns";
-      src = lib.cleanSource ./.;
-    };
+  validate = depot.tools.checks.validateTerraform {
+    inherit terraform;
+    name = "dns";
+    src = lib.cleanSource ./.;
   };
 }
